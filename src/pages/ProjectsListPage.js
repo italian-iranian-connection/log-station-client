83df31a09f443ebaf38a72b091e3b7e94641f31d
Alessandra Scarpellini <scaalessandra@icloud.com>
2022-11-01 00:57:42 +0100

    minus change

--- src/pages/ProjectsListPage.js
+++ src/pages/ProjectsListPage.js
@@ -3,75 +3,75 @@ import { useState, useEffect, useContext } from "react";
 import RiseLoader from "react-spinners/RiseLoader";
 import AddProject from "../components/AddProject";
 import ProjectCard from "../components/ProjectCard";
-import { AuthContext } from '../context/auth.context';
+import { AuthContext } from "../context/auth.context";
 import { Link } from "react-router-dom";
 import LoginPage from "./LoginPage";
 
-
 const API_URL = "http://localhost:5005";
 
-function ProjectsListPage(){
-    const [projects, setProjects] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const style = { textDecoration: "none", color: "fuchsia" };
+function ProjectsListPage() {
+  const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const style = { textDecoration: "none", color: "fuchsia" };
 
-    const { isLoggedIn } = useContext(AuthContext);
+  const { isLoggedIn } = useContext(AuthContext);
 
-    const getAllProjects = ()=>{
-        const storedToken = localStorage.getItem("authToken");
-        axios.get(`${API_URL}/api/projects`, { headers: { Authorization: `Bearer ${storedToken}` } })
-        .then(response=>{
-            setProjects(response.data)
-            setLoading(false)
-        })
-        .catch((error) => console.log(error));
-    }
+  const getAllProjects = () => {
+    const storedToken = localStorage.getItem("authToken");
+    axios
+      .get(`${API_URL}/api/projects`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      .then((response) => {
+        setProjects(response.data);
+        setLoading(false);
+      })
+      .catch((error) => console.log(error));
+  };
 
-    useEffect(()=>{
-        getAllProjects();
-    }, [])
+  useEffect(() => {
+    getAllProjects();
+  }, []);
 
-    return(
-        <div className="ProjectsListPage">
-        <RiseLoader
+  return (
+    <div className="ProjectsListPage">
+      <RiseLoader
         color="fuchsia"
         loading={loading}
         size={200}
         aria-label="Loading Spinner"
         data-testid="loader"
       />
-            <div className="container">
-                <div className="row mt-2">
-                    <div className="col">
-                       {projects.map((project) => {
-                         return (
-                           <ProjectCard {...project} key={project._id}/>
-                         )
-                       })}
-                    </div>
-                    {
-                    isLoggedIn
-                    ? 
-                    <div className="col-12 col-lg-6">
-                    <AddProject />  
-                    </div>
-                    : 
-                    <div className="col-12 col-lg-6 mt-2">
-                      <div className="card p-5">
-                      <Link to="/signup" style={style}>
-                      <h1 className="danger">Sign Up and start collaborating with other developers like you!</h1>
-                      </Link>
-                      </div>
-                      <div className="card p-5 mt-2">
-                        <LoginPage />
-                      </div> 
-                    </div>
-                    }
-                </div>
+      <div className="container">
+        <div className="row mt-2">
+          <div className="col">
+            {projects.map((project) => {
+              return <ProjectCard {...project} key={project._id} />;
+            })}
+          </div>
+          {isLoggedIn ? (
+            <div className="col-12 col-lg-6">
+              <AddProject />
             </div>
+          ) : (
+            <div className="col-12 col-lg-6 mt-2">
+              <div className="card p-5">
+                <Link to="/signup" style={style}>
+                  <h1 className="danger">
+                    Sign Up and start collaborating with other developers like
+                    you!
+                  </h1>
+                </Link>
+              </div>
+              <div className="card p-5 mt-2">
+                <LoginPage />
+              </div>
+            </div>
+          )}
         </div>
-    )
-
+      </div>
+    </div>
+  );
 }
 
 export default ProjectsListPage;
